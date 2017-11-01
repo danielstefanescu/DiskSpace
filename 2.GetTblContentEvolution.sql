078270c07cd9c88848493cc65c6f77a6f6673d4f
ds19 <danielstefanescu@hotmail.com>
2017-11-01 11:31:12 +0100

    Add files via upload

---
+++ 2.GetTblContentEvolution.sql
@@ -0,0 +1,11 @@
+﻿
+SELECT DATEPART(yyyy, CreationDate) AS [year],
+       DATEPART(mm, CreationDate) AS [month],
+       COUNT(*) AS [count],
+       SUM(DATALENGTH(Content)) / 1048576.0 / 1024.0 AS [SizeInGB],
+       (SUM(DATALENGTH(Content)) / 1048576.0 / 1024.0) / COUNT(*) AS [AverageSize]
+FROM tbl_Content
+GROUP BY DATEPART(yyyy, CreationDate),
+         DATEPART(mm, CreationDate)
+ORDER BY DATEPART(yyyy, CreationDate),
+         DATEPART(mm, CreationDate);
